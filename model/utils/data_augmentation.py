cc9e0fc19d8dc3247727925b087ac676138a38b5
Eike <eike_kutzki@gmx.de>
2019-12-26 12:23:22 +0100

    adding unNormalization

--- model/utils/data_augmentation.py
+++ model/utils/data_augmentation.py
@@ -112,3 +112,20 @@ class RandomCrop(object):
 
     def __repr__(self):
         return self.__class__.__name__ + '(size={0}, padding={1})'.format(self.size, self.padding)
+
+class UnNormalize(object):
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, tensor):
+        """
+        Args:
+            tensor (Tensor): Tensor image of size (C, H, W) to be normalized.
+        Returns:
+            Tensor: Normalized image.
+        """
+        for t, m, s in zip(tensor, self.mean, self.std):
+            t.mul_(s).add_(m)
+            # The normalize code -> t.sub_(m).div_(s)
+        return tensor
